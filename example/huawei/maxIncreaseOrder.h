90bbda584fba9aaa43b869189f840fc2a885ecae
sunbinbin1991 <1037790341@qq.com>
2020-03-23 23:44:04 +0800

    add get maxIncreaseOrder

--- example/huawei/maxIncreaseOrder.h
+++ example/huawei/maxIncreaseOrder.h
@@ -31,12 +31,38 @@ void get_maxContinuousIncreaseOrder(const std::vector<int>& nums) {
 
 }
 
+void get_maxIncreaseOrder(const std::vector<int>& nums) {
+	std::vector<int> target(nums.size());
+	target[0] = 1;
+	for (size_t i = 0; i < nums.size(); i++)
+	{
+		printf("nums = %d\n", nums[i]);
+		target[i] = 1;
+	}
+	for (int i = 0; i < nums.size(); i++)
+	{
+		for (int j = 0; j < i; j++) {
+			if (nums[j] < nums[i]) {
+				target[i] = std::max(target[j]+1, target[i]);
+			}
+			//target[i] = std::max(target[j], target[i]);
+		}
+		printf("target = %d\n", target[i]);
+	}
+	for (int i = 1; i < nums.size(); i++)
+	{
+		//printf("target %d = %d\n", i,target[i]);
+	}
+
+}
+
 int test_maxContinuousIncreaseOrder() {
 	std::vector<int> nums;
 	int num;
 	while (cin >> num) {
 		nums.push_back(num);
 	}
-	get_maxContinuousIncreaseOrder(nums);
+	//get_maxContinuousIncreaseOrder(nums);
+	get_maxIncreaseOrder(nums);
 	return 0;
 }
