bccb46f25247f181c6b068b45547411f1fcad6f0
sunbinbin1991 <1037790341@qq.com>
2020-05-25 07:54:04 +0800

    8 字符串转int-状态机

--- example/leetCode/8_atoi.h
+++ example/leetCode/8_atoi.h
@@ -1,6 +1,34 @@
 #pragma once
 #include<iostream>
 using namespace std;
+
+class Solution {
+public:
+	int myAtoi(string str)
+	{
+		long long int ans = 0;
+		int flag = 0;//用每一位分别存储状态信息，初始位0000（只用到四位）
+		for (int i = 0; i<str.size(); i++) {
+			if ((flag & 1) == 0 && str[i] == ' ')
+				continue;
+			flag |= 1;//此时位0001，代表空字符判断结束
+			if ((flag & 14) == 0 && (str[i] == '+' || str[i] == '-')) {//14=1110，3个1分别代表出现数字、符号位为“+”和符号位为“-”。显然需要这三位都为0才可以进行符号位判断
+				flag |= (str[i] == '-' ? 2 : 4);//'+'则和0100取或，加上4'-'则和0010取或
+				continue;
+			}
+			if (str[i] >= '0' && str[i] <= '9') {
+				flag |= 8;
+				ans = 10 * ans + str[i] - '0';//'0'用来将ascll码转换成数字（利用其排序规则）
+				if (ans>(long long)INT_MAX)//每加一位数字都进行溢出判断，防止数字过大
+					return (flag & 2) == 2 ? INT_MIN : INT_MAX;
+				continue;
+			}
+			break;//判断首次出现数字后是否出现非数字字符，出现则结束循环
+		}
+		return (flag & 2) == 2 ? -ans : ans;
+	}
+};
+
 int test_atoi()
 {
 	string str;
