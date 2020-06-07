57793f3b08bd45309381a4628dfb0f17f57bc8b5
sunbinbin1991 <1037790341@qq.com>
2020-06-07 16:40:15 +0800

    6 z转换-let

--- example/leetCode/6_zconvert.h
+++ example/leetCode/6_zconvert.h
@@ -8,6 +8,28 @@ enum ZDirection{
 	upright = 1,
 };
 
+class Solution_zconvert {
+public:
+	string convert(string s, int numRows)
+	{
+
+		if (numRows == 1) return s;
+		vector<string> rows(min(numRows, int(s.size())));
+		int curRow = 0;
+		bool goingDown = false;
+
+		for (char c : s) {
+			rows[curRow] += c;
+			if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+			curRow += goingDown ? 1 : -1;
+		}
+
+		string ret;
+		for (string row : rows) ret += row;
+		return ret;
+	}
+};
+
 void zconvert(string& input, int rows)
 {
 	if (input.empty()) {
