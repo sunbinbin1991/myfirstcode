49cbadd7effbbcda3c0d0121e166810355da21a0
sunbinbin1991 <1037790341@qq.com>
2020-07-22 08:26:36 +0800

    删除重复数字+1

--- example/leetCode/26_removeDuplicates.h
+++ example/leetCode/26_removeDuplicates.h
@@ -1,5 +1,18 @@
 #pragma once
 #include<vector>
+int rmDep(vector<int>& nums)
+{
+    if (nums.size() == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.size(); j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
+
 int removeDuplicates(vector<int>& nums)
 {
 	vector<int> res;
