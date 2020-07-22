c748e5401200bc9052ec45e844b9e718a83feaf4
sunbinbin1991 <1037790341@qq.com>
2020-07-22 23:57:48 +0800

    删除列表中给定数组

--- example/leetCode/26_removeDuplicates.h
+++ example/leetCode/26_removeDuplicates.h
@@ -1,5 +1,18 @@
 #pragma once
 #include<vector>
+int removeElement(vector<int>& nums, int val)
+{
+    int len = nums.size();
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        if (nums[i] != val) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+}
+
 int rmDep(vector<int>& nums)
 {
     if (nums.size() == 0) return 0;
