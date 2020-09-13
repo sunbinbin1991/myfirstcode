9eee451989378505a82b0273bb203d50564ade80
sunbinbin1991 <1037790341@qq.com>
2020-09-14 00:27:21 +0800

    DFS 堆栈遍历 error1

--- example/leetCode/104_maxDepth.h
+++ example/leetCode/104_maxDepth.h
@@ -27,8 +27,8 @@ namespace tree {
          3
         / \
        9  20
-         / \
-       15   7 
+         /  \
+        15   7 
     */
 
     vector<TreeNode> traversal(TreeNode root)
@@ -50,13 +50,37 @@ namespace tree {
         return res;
     }
 
+    void DFS(TreeNode *root, int level,  vector<vector<int>> &res) 
+    {
+        if (root == nullptr) {
+            return;
+        }
+
+        if (res.empty()) {
+            vector<int> tmp;
+            tmp.push_back(root->val);
+            res.push_back(tmp);
+        } else {
+            printf(" test %d %d \n", res.size(),level);
+            if (res.size() == level) {
+                vector<int> tmp;
+                tmp.push_back(root->val);
+                res.push_back(tmp);
+            } else {
+                res[res.size() - 1].push_back(root->val);
+            }
+        }
+        DFS(root->left, level + 1, res);
+        DFS(root->right, level + 1, res);
+    }
+
     TreeNode* generateTree() {
         TreeNode* root = new TreeNode(3);
         root->left = new TreeNode(9);
         root->right = new TreeNode(20);
-        root->right->left = new TreeNode(15);
-        root->right->right = new TreeNode(7);
-        root->right->right->right = new TreeNode(7);
+        root->left->left = new TreeNode(15);
+        root->left->right = new TreeNode(7);
+        //root->right->right->right = new TreeNode(7);
         return root;
     }
 
@@ -71,11 +95,26 @@ namespace tree {
     void test_maxdepth() 
     {
         TreeNode *root = generateTree();
+        // 1: 获取节点的深度
         int dep = maxDepth(root);
 
+        // 2：遍历所有节点
         vector<TreeNode> res = traversal(*root);
         for (size_t i = 0; i < res.size(); i++) {
-            printf("%d\n", res[i].val);
+            //printf("%d\n", res[i].val);
         }
+
+        // 3：层次遍历所有节点
+        vector<vector<int>> resvec;
+        DFS(root, 0, resvec);
+        for (size_t i = 0; i < resvec.size(); i++) {
+            for (size_t j = 0; j < resvec[i].size(); j++) {
+                printf("%d ", resvec[i][j]);
+            }
+            printf("\n");
+        }
+
     }
+
+    
 }
