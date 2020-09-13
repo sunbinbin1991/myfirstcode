cd0d0fb5fe5cb582df783b228d35d8560f82fdf4
sunbinbin1991 <1037790341@qq.com>
2020-09-13 14:07:15 +0800

    add commit

--- example/leetCode/120_minimumTotal.h
+++ example/leetCode/120_minimumTotal.h
@@ -8,32 +8,40 @@ using namespace std;
 int minimumTotal(vector<vector<int>>& triangle)
 {
     if (triangle.empty()) return 0;
+    if (triangle.size() == 1) return triangle[0][0];
     int len = triangle.size();
     vector<vector<int>> DP(len + 1, vector<int>(len + 1));
 
     DP[0][0] = triangle[0][0];
-    for (int i = 1; i < len; i++) {
-        //DP[i][0] = triangle[i - 1][0] + DP[i - 1][0];
-    }
+    DP[1][0] = triangle[0][0] + triangle[1][0];
+    DP[1][1] = triangle[0][0] + triangle[1][1];
 
-    for (int i = 1; i < len; i++) {
-        for (size_t j = 0; j < i-1; j++) {
-            if (triangle[i-1][j + 1] < triangle[i-1][j]) {
-                DP[i][j] = DP[i-1][j] + triangle[i - 1][j + 1];
+    for (int i = 2; i < len; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0) {
+                DP[i][0] = DP[i - 1][0] + triangle[i][0];
+            } else if (j == i) {
+                DP[i][j] = DP[i - 1][j - 1] + triangle[i][j];
             } else {
-                DP[i][j] = DP[i - 1][j] + triangle[i - 1][j];
+                if (DP[i - 1][j - 1] < DP[i - 1][j]) {
+                    DP[i][j] = DP[i - 1][j - 1] + triangle[i][j];
+                } else {
+                    DP[i][j] = DP[i - 1][j] + triangle[i][j];
+                }
             }
         }
     }
-
-    for (int i = 1; i < len; i++) {
-        for (size_t j = 0; j < i; j++) {
+    int minRes = DP[len-1][0];
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j <= i; j++) {
             printf(" %d", DP[i][j]);
         }
         printf(" \n");
     }
-
-    return 0;
+    for (int i = 0; i < len; i++) {
+        minRes = min(minRes, DP[len - 1][i]);
+    }
+    return minRes;
 }
 
 void test_minimumTotal(){
