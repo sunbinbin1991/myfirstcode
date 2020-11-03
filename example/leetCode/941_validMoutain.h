25bd953c33b060cdd11b387004a79a7fd6014003
sunbinbin1991 <1037790341@qq.com>
2020-11-03 08:44:36 +0800

    山型数组判定是否存在

---
+++ example/leetCode/941_validMoutain.h
@@ -0,0 +1,29 @@
+#include<vector>
+#include<cmath>
+using namespace std;
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A)
+    {
+        int N = A.size();
+        int i = 0;
+
+        // 递增扫描
+        while (i + 1 < N && A[i] < A[i + 1]) {
+            i++;
+        }
+
+        // 最高点不能是数组的第一个位置或最后一个位置
+        if (i == 0 || i == N - 1) {
+            return false;
+        }
+
+        // 递减扫描
+        while (i + 1 < N && A[i] > A[i + 1]) {
+            i++;
+        }
+
+        return i == N - 1;
+    }
+};
+
