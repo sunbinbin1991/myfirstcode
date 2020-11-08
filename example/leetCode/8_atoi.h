f8f82e7c57c0ff4f5e3cdb5fe12a5e744e28520a
sunbinbin1991 <1037790341@qq.com>
2020-11-08 09:07:48 +0800

    ATOI 实现

--- example/leetCode/8_atoi.h
+++ example/leetCode/8_atoi.h
@@ -4,6 +4,7 @@ using namespace std;
 
 class Solution_atoi {
 public:
+
 	int myAtoi(string str)
 	{
 		long long int ans = 0;
@@ -29,13 +30,37 @@ public:
 	}
 };
 
+int myAtoi(string str)
+{
+    if (str.empty()) return 0;
+
+    int flag = 1;
+    int startIndex = 0;
+    string subString = "";
+    int i = 0;
+    while (str[i] == ' ') {
+        i++;
+    }
+    if (str[i] == '+') flag = 1;
+    if (str[i] == '-') flag = -1;
+    int sum = 0;
+    if (str[i] == '-' || str[i] == '+') i++;
+    while (i < str.size() && isdigit(str[i])) {
+        int res = str[i] - '0';
+        if (sum > INT_MAX / 10 || sum == INT_MAX && res > 7) {
+            return flag > 0 ? INT_MAX : INT_MIN;
+        }
+        sum = 10 * sum + res;
+        i++;
+    }
+    return sum * flag;
+}
+
+
 int test_atoi()
 {
-	string str;
-	while (cin >> str) {
-		int res = atoi(str.c_str());
-		cout << "res = " << res;
-	}
+    string str = "-42";
+    int res = myAtoi(str);
 	return 0;
 	
 }
