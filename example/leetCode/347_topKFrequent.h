364fa4afdfea25a03cc3ef226a8e6d2204933d4d
sunbinbin1991 <1037790341@qq.com>
2021-08-15 22:18:39 +0800

    使用小堆队列 更慢了

--- example/leetCode/347_topKFrequent.h
+++ example/leetCode/347_topKFrequent.h
@@ -3,11 +3,16 @@
 #include<algorithm>
 #include<iostream>
 #include<map>
+#include<queue>
 using namespace std;
 struct numFrq
 {
     int num;
     int frq;
+    bool operator<(const numFrq& a) const
+    {
+         return frq > a.frq; //Ð¡¶¥¶Ñ
+    }
 };
 
 bool compFrq(numFrq n1, numFrq n2) {
@@ -19,6 +24,7 @@ vector<int> topKFrequent(vector<int>& nums, int k)
     if (nums.empty()) return{};
 
     vector<numFrq> numsFrq;
+    priority_queue<numFrq> numFrqQue;
     map<int, int> numsFrqMap;
     for (size_t i = 0; i < nums.size(); i++) {
         if (numsFrqMap.find(nums[i]) != numsFrqMap.end()) {
@@ -28,14 +34,29 @@ vector<int> topKFrequent(vector<int>& nums, int k)
         }
     }
     for (auto itr: numsFrqMap) {
-        numsFrq.push_back(numFrq{ itr.first, itr.second });
+        if (numFrqQue.empty() || numFrqQue.size() <= k) {
+            numFrqQue.push(numFrq{ itr.first, itr.second });
+        } else {
+            auto top = numFrqQue.top();
+            if (top.frq < itr.second) {
+                numFrqQue.pop();
+                numFrqQue.push(numFrq{ itr.first, itr.second });
+            }
+        }
+
+        //numsFrq.push_back(numFrq{ itr.first, itr.second });
     }
 
-    sort(numsFrq.begin(), numsFrq.end(), compFrq);
+    //sort(numsFrq.begin(), numsFrq.end(), compFrq);
     vector<int> ans;
-    for (size_t i = 0; i < numsFrq.size() && i < k; i++) {
-        cout << numsFrq[i].num << " : " << numsFrq[i].frq << endl;
-        ans.push_back(numsFrq[i].num);
+    while (!numFrqQue.empty()) {
+        auto top = numFrqQue.top();
+        ans.push_back(top.num);
+        numFrqQue.pop();
     }
+    //for (size_t i = 0; i < numsFrq.size() && i < k; i++) {
+    //    cout << numsFrq[i].num << " : " << numsFrq[i].frq << endl;
+    //    ans.push_back(numsFrq[i].num);
+    //}
     return ans;
 }
