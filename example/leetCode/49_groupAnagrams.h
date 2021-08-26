2f8593badb054c65bcd922a6373427eb3ed79dfd
Binbin sun <1037790341@qq.com>
2021-08-26 16:19:31 +0800

    Create 49_groupAnagrams.h

---
+++ example/leetCode/49_groupAnagrams.h
@@ -0,0 +1,14 @@
+// 字符串排序后,字符串即成唯一    
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string& str: strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            mp[key].emplace_back(str);
+        }
+        vector<vector<string>> ans;
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            ans.emplace_back(it->second);
+        }
+        return ans;
+    }
