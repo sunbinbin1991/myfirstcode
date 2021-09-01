24edf6e43b9cfcfdc4314becbe93ac52b7a35a98
Binbin sun <1037790341@qq.com>
2021-09-01 21:04:50 +0800

    Update 76_minWindow.h

--- example/leetCode/76_minWindow.h
+++ example/leetCode/76_minWindow.h
@@ -1,3 +1,50 @@
+string minWindow(string s, string t) {
+    int left = 0;
+    int right = 0;
+    int start  = 0;
+    int minStrLen = INT_MAX;
+     // 记录最小覆盖子串的起始索引及长度
+    unordered_map<char, int> need, window;
+    for(int i = 0; i < t.size(); i++){
+        need[t[i]] ++;
+    }
+    int valid = 0;
+    while (right < s.size()) {
+         // c 是将移入窗口的字符
+        char c = s[right];
+        // 右移窗口
+        right++;
+        // 进行窗口内数据的一系列更新
+        if(need.count(c)){
+            window[c]++;
+            if(window[c] == need[c]){
+                valid++;
+            }
+        }
+        
+        // 判断左侧窗口是否要收缩
+        while (valid == need.size()) {
+         // 在这里更新最小覆盖子串
+            if(right - left < minStrLen){
+                start = left;
+                minStrLen = right - left;
+            }
+            // d 是将移出窗口的字符
+            char d = s[left];
+             // 左移窗口
+            left++;
+             // 进行窗口内数据的一系列更新
+            if(need.count(d)){
+                if(window[d] == need[d]){
+                    valid--;
+                }
+                window[d]--;
+            }
+        }
+    }
+    // 返回最小覆盖子串
+    return minStrLen == INT_MAX ? "" : s.substr(start, minStrLen);
+}
 string minWindow_TimeLimited(string s, string t) {
     int left = 0;
     int right = 0;
