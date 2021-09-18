d1dd2ac5ec085152f6e760448181797c6ef495d4
Binbin sun <1037790341@qq.com>
2021-09-18 10:12:30 +0800

    计算子叶子节点的和

--- example/leetCode/404_sumOfLeftLeaves.h
+++ example/leetCode/404_sumOfLeftLeaves.h
@@ -19,3 +19,25 @@
         res += helper(root);
         return res;
     }
+class Solution404 {
+public:
+    bool isLeafNode(TreeNode* node) {
+        return !node->left && !node->right;
+    }
+
+    int dfs(TreeNode* node) {
+        int ans = 0;
+        if (node->left) {
+            ans += isLeafNode(node->left) ? node->left->val : dfs(node->left);
+        }
+        if (node->right && !isLeafNode(node->right)) {
+            ans += dfs(node->right);
+        }
+        return ans;
+    }
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        return root ? dfs(root) : 0;
+    }
+};
+
