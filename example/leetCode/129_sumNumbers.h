a0ab3bb203c72997e544edd1c96594a1fdf9c4f7
Binbin sun <1037790341@qq.com>
2021-09-18 16:15:49 +0800

    更新了官方版本

--- example/leetCode/129_sumNumbers.h
+++ example/leetCode/129_sumNumbers.h
@@ -5,6 +5,23 @@
 #ifndef MYFIRSTCODE_129_SUMNUMBERS_H
 #define MYFIRSTCODE_129_SUMNUMBERS_H
 
+class Solution129 {
+public:
+    int dfs(TreeNode* root, int prevSum) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root->val;
+        if (root->left == nullptr && root->right == nullptr) {
+            return sum;
+        } else {
+            return dfs(root->left, sum) + dfs(root->right, sum);
+        }
+    }
+    int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);
+    }
+};
 
 void dfsPathMine_WRONG(set<string>& paths, string& path, TreeNode* root){
     if(root == nullptr){
