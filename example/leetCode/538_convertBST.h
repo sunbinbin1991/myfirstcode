c514aa547469ef0cfffdb1a2cad5147ffd794b9a
Binbin sun <1037790341@qq.com>
2021-11-02 11:31:47 +0800

    二叉树转累加树，累加树，中序遍历的逆遍历

---
+++ example/leetCode/538_convertBST.h
@@ -0,0 +1,14 @@
+class Solution538 {
+public:
+    int sum = 0;
+
+    TreeNode* convertBST(TreeNode* root) {
+        if (root != nullptr) {
+            convertBST(root->right);
+            sum += root->val;
+            root->val = sum;
+            convertBST(root->left);
+        }
+        return root;
+    }
+};
